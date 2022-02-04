1e149c8586fd18bd9b41dbe137bb223d52b1f431
Ilya Sh <shefchenkornd@gmail.com>
2022-02-04 08:01:23 +0300

    configure logger in App core

--- internal/app/rest_api/config.go
+++ internal/app/rest_api/config.go
@@ -3,11 +3,13 @@ package rest_api
 // Config General instance for App
 type Config struct {
 	BindAddr string	`toml:bind_addr`
+	LoggerLevel string `toml:logger_level`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		LoggerLevel: ":debug",
 	}
 }
 

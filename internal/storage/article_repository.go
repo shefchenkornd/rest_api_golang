4cf60d4396101f3f3e257bd8873e524220b129f9
Ilya Sh <shefchenkornd@gmail.com>
2022-02-11 22:48:55 +0300

    create repository for Article model

--- internal/storage/article_repository.go
+++ internal/storage/article_repository.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"github.com/shefchenkornd/rest_api/internal/models"
@@ -16,9 +17,9 @@ var (
 	tableArticle = "articles"
 )
 
-// Create article
+// Create new article
 func (ar *ArticleRepository) Create(model *models.Article) (*models.Article, error) {
-	query := fmt.Sprintf("INSERT INTO %s (title, author, content) VALUES ($1, $2, $3) RETURNING id", tableUser)
+	query := fmt.Sprintf("INSERT INTO %s (title, author, content) VALUES ($1, $2, $3) RETURNING id", tableArticle)
 	if err := ar.storage.db.QueryRow(query, model.Title, model.Author, model.Content).Scan(&model.Id); err != nil {
 		return nil, err
 	}
@@ -26,43 +27,37 @@ func (ar *ArticleRepository) Create(model *models.Article) (*models.Article, err
 	return model, nil
 }
 
-// DeleteById ...
-func (ar *ArticleRepository) DeleteById(id int) (*models.Article, error) {
-	article, found, err := ar.FindById(id)
-	if err != nil {
-		return nil, err
-	}
-
-	if !found {
-		return nil, errors.New("article not found")
-	}
-
-	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1;", tableUser)
-	_, err = ar.storage.db.Query(query, id)
-	if err != nil {
-		return nil, err
-	}
-
-	return article, nil
-}
-
-// FindById ...
+// FindById find article by id
 func (ar *ArticleRepository) FindById(id int) (*models.Article, bool, error) {
-	var article *models.Article
 	founded := false
 
-	query := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", tableUser)
-	if err := ar.storage.db.QueryRow(query, id).Scan(&article); err != nil {
-		return nil, false, err
+	query := fmt.Sprintf("SELECT title, author, content FROM %s WHERE id=$1", tableArticle)
+	var title string
+	var author string
+	var content string
+	if err := ar.storage.db.QueryRow(query, id).Scan(&title, &author, &content); err != nil {
+		// если нет результатов из БД, то это не ошибка!
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, founded, nil
+		}
+
+		return nil, founded, err
 	}
 	founded = true
+
+	article := &models.Article{
+		Id:      id,
+		Title:   title,
+		Author:  author,
+		Content: content,
+	}
 
 	return article, founded, nil
 }
 
-// SelectAll all articles
+// SelectAll select all articles
 func (ar *ArticleRepository) SelectAll() ([]*models.Article, error) {
-	query := fmt.Sprintf("SELECT * FROM %s", tableUser)
+	query := fmt.Sprintf("SELECT id, title, author, content FROM %s", tableArticle)
 	rows, err := ar.storage.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -72,13 +67,42 @@ func (ar *ArticleRepository) SelectAll() ([]*models.Article, error) {
 	// Подготовим куда, будем читать данные
 	articles := make([]*models.Article, 0)
 	for rows.Next() {
-		article := &models.Article{}
-		if err := rows.Scan(article.Id, article.Title, article.Author, article.Content); err != nil {
+		var id int
+		var title string
+		var author string
+		var content string
+		if err := rows.Scan(&id, &title, &author, &content); err != nil {
 			log.Println(err)
 			continue
+		}
+		article := &models.Article{
+			Id:      id,
+			Title:   title,
+			Author:  author,
+			Content: content,
 		}
 		articles = append(articles, article)
 	}
 
 	return articles, nil
+}
+
+// DeleteById delete article by id
+func (ar *ArticleRepository) DeleteById(id int) (*models.Article, bool, error) {
+	article, found, err := ar.FindById(id)
+	if err != nil {
+		return nil, false, err
+	}
+
+	if !found {
+		return nil, false, nil
+	}
+
+	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1;", tableArticle)
+	_, err = ar.storage.db.Query(query, id)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return article, found,  nil
 }
